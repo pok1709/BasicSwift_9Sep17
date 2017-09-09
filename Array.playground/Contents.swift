196a8b3f85a4c4d1d6eb383a25f025df3bf47331
pok1709 <pok1709@gmail.com>
2017-09-09 14:37:27 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Array Format
+
+var numberAInts = [11,22,33,44,55]
+
+
+print("Length of Array : \(numberAInts.count)")
+
+print("Value of Index 2 : \(numberAInts[2])")
+
+print("Value of Index 4 : \(numberAInts[4])")
+
+//Add new element to last
+numberAInts.append(66)
+
+
+//Delete element
+numberAInts.remove(at: 2)
+
+numberAInts
+
+
+
+
+

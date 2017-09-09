d33e1eb569f02c75139e4a70b125196b387805a1
pok1709 <pok1709@gmail.com>
2017-09-09 10:14:31 +0700

    test

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Declare Variable on Swift
+//Implicit แบบไม่กำหนด Data Type
+
+var nameString = "Sorapong"
+
+nameString = "Doramon"
+
+//Constant
+
+let surnameString = "ABCD"
+
+//surnameString = "XXX" // error เพราะ let เปลี่ยนค่าไม่ได้
+
+
+
+
+
+
+
